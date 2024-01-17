aa00b612755cb2f4979cbef75ff7a7aadcdee637
DriSidibe <dsidibe653@gmail.com>
2024-01-17 17:28:36 +0000

    0x16-api_advanced

--- 0x14-mysql/create-db.sql
+++ 0x14-mysql/create-db.sql
@@ -1,13 +1,15 @@
 -- Create db on the master server
 CREATE DATABASE IF NOT EXISTS tyrell_corp;
 
+use tyrell_corp;
+
 -- Create table
-CREATE TABLE IF NOT EXISTS tyrell_corp.nexus6
+CREATE TABLE IF NOT EXISTS nexus6
 (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id),
+	PRIMARY KEY (id)
 );
 
 -- Insert an entry
-INSERT INTO `tyrell_corp.nexus6` (`name`) VALUES ("Emmastro");
+INSERT INTO `nexus6` (`name`) VALUES ("Emmastro");
