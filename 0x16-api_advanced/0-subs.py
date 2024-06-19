83367563d2d55f5a213d241e5f077085fc1bc4b5
DriSidibe <dsidibe653@gmail.com>
2024-06-19 22:36:32 +0000

    update 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,20 +7,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    # Set up the base URL and headers
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'python:subreddit.subscriber.counter:v1.0 (by /u/yourusername)'}
-    
-    try:
-        # Make the GET request
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if the status code indicates success
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
-        # If any request exception occurs, return 0
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/drissa)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
